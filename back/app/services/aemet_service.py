d1665ab5b2962ed3a5f746b47c1d11ee6736dc81
JavierGarciaGarcia524 <javgar020104@gmail.com>
2025-08-13 23:53:26 +0200

    Create backend structure

---
+++ back/app/services/aemet_service.py
@@ -0,0 +1,10 @@
+import requests
+
+def get_weather_data():
+    """Obtain all data from aemet"""
+    # url = "https://opendata.aemet.es/opendata/api/prediccion/nacional/24horas/"
+    # headers = {'api_key': 'TU_API_KEY_AQUI'}
+    
+    # response = requests.get(url, headers=headers)
+    #return response.json() if response.status_code == 200 else {'error': 'API call failed'}
+    return None  # Placeholder for actual implementation
